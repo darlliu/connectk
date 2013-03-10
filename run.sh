aec668e4ecb962de88ffee5616887ffe961170f1
darlliu <darlliu@github.com>
2013-03-10 15:56:24 -0700

    compiled files change

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-java -jar ConnectKShell.jar processAI ./src/a.out
+#java -jar ConnectKShell.jar processAI ./src/a.out
+java -jar CKTournament.jar DummyAI.class cpp:./a.out
+rm -f core.*
+
--- src/compile.sh
+++ src/compile.sh
@@ -1,1 +1,2 @@
 g++ -o a.out main.cpp master.cpp headers.h master.h utils.h -O3 -std=c++11
+mv a.out ../
